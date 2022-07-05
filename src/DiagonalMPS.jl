639d1a6568bd0c3c9a856d03836d06978b50d420
Daniel Torres <torres@lipn.univ-paris13.fr>
2022-07-05 16:27:59 +0200

    Functions update.

--- src/DiagonalMPS.jl
+++ src/DiagonalMPS.jl
@@ -4,6 +4,11 @@ using Printf
 abstract type AbstractDiagonalMPS end
 
 """
+Enum to set diagonal numbers as 1 or 2.
+"""
+@enum DNumber First=1 Second=2
+
+"""
 A DiagonalMPS structure represents a set of Matrix X_{k,N}[1] or X_{k,N}[2]
 into the MPS diagonal structure, each of size (BlockSizes[i][1] X BlockSizes[i][2])
 and position indices (BlockIndex[i][1] X BlockIndex[i][2]). Diagonal number (1 or 2)
@@ -14,29 +19,32 @@ mutable struct DiagonalMPS <: AbstractDiagonalMPS
     DiagNumber::UInt8
     NBlocks::UInt16
     X::Array{Matrix{Float64}}
-    BlockIndex::Array{Tuple{UInt16, UInt16}}
+    BlockIndex::Array{Tuple{UInt16, UInt16}, 1}
     BlockSizes::Array{Tuple{UInt16, UInt16}}
 
     """
-    diagMPS = DiagonalMPS(1, 2, [[1.0 2.0; 3.0 4.0], [1.0 2.0 3.0; 4.0 5.0 6.0; 7.0 8.0 9.0]], [(1, 1), (2, 2)], [(2, 2), (3, 3)])
+    diagMPS = DiagonalMPS(1, 2, [[1.0 2.0; 3.0 4.0], [1.0 2.0 3.0; 4.0 5.0 6.0]], [(1, 1), (2, 2)], [(2, 2), (2, 3)])
 
     Constructs a new DiagonalMPS structure with diagonal number 1 and 2 sub-blocks:
-    the first one of size 2 X 2, the second one of size 3 X 3.
+    the first one of size 2 X 2, the second one of size 2 X 3.
     Elements in the sub-blocks are set to the matrices provided as third argument
     and indices are set to the array provided as fourth argument.
     """
     function DiagonalMPS(diagN, nblocks, x, indices, sizes)
-        new(diagN, nblocks, x, indices, sizes)
+        dNumber = convert(UInt8, diagN)
+        dNumber = (dNumber == UInt8(First) || dNumber == UInt8(Second)) ? dNumber : (dNumber < UInt8(First)) ? UInt8(First) : UInt8(Second)
+        new(dNumber, nblocks, x, indices, sizes)
     end
 end
 
 """
-diagMPS = DiagonalMPS(1, 2, [(2, 2), (3, 3)])
+diagMPS = DiagonalMPS(1, 2, [(2, 2), (2, 3)])
 
 Constructs a new DiagonalMPS structure with diagonal number 1 and 2 sub-blocks:
-the first one of size 2 X 2, the second one of size 3 X 3.
+the first one of size 2 X 2, the second one of size 2 X 3.
 Elements in the sub-blocks are set to 0 and indices in BlockIndex array are set
-to (i,i) (i.e. (1, 1), (2, 2), ...).
+to (i, i) if diagonal number is 1 (i.e. (1, 1), (2, 2), ...) or (i, i+1) otherwise
+(i.e. (1, 2), (2, 3), ...).
 """
 function DiagonalMPS(diagN, nblocks, sizes)
 #=     if !isinteger(diagN)
@@ -55,13 +63,14 @@ function DiagonalMPS(diagN, nblocks, sizes)
     end
  =#
     dNumber = convert(UInt8, diagN)
+    dNumber = (dNumber == UInt8(First) || dNumber == UInt8(Second)) ? dNumber : (dNumber < UInt8(First)) ? UInt8(First) : UInt8(Second)
     nBlocks = convert(UInt16, nblocks)
     bSizes = convert(Array{Tuple{UInt16, UInt16}}, sizes)
 
     X_ = Array{Matrix{Float64}}(undef, nBlocks)
-    indices = Array{Tuple{UInt16, UInt16}}(undef, nBlocks)
+    indices = Array{Tuple{UInt16, UInt16}, 1}(undef, nBlocks)
     for i = 1 : nBlocks
-        indices[i] = (i, i)
+        indices[i] = (i, dNumber == UInt8(First) ? i : i + 1)
         X_[i] = Matrix(zeros(bSizes[i][1], bSizes[i][2]))
     end
 
@@ -69,12 +78,13 @@ function DiagonalMPS(diagN, nblocks, sizes)
 end
 
 """
-diagMPS = DiagonalMPS(1, 2, [[1.0 2.0; 3.0 4.0], [1.0 2.0 3.0; 4.0 5.0 6.0; 7.0 8.0 9.0]], [(2, 2), (3, 3)])
+diagMPS = DiagonalMPS(1, 2, [[1.0 2.0; 3.0 4.0], [1.0 2.0 3.0; 4.0 5.0 6.0]], [(2, 2), (2, 3)])
 
 Constructs a new DiagonalMPS structure with diagonal number 1 and 2 sub-blocks:
-the first one of size 2 X 2, the second one of size 3 X 3.
+the first one of size 2 X 2, the second one of size 2 X 3.
 Elements in the sub-blocks are set to the matrices provided as third argument
-and indices in the BlockIndex array are set to (i,i) (i.e. (1, 1), (2, 2), ...).
+and indices in the BlockIndex array are set to (i, i) if diagonal number is 1
+(i.e. (1, 1), (2, 2), ...) or (i, i+1) otherwise (i.e. (1, 2), (2, 3), ...).
 """
 function DiagonalMPS(diagN, nblocks, x, sizes)
 #=     if !isinteger(diagN)
@@ -98,26 +108,29 @@ function DiagonalMPS(diagN, nblocks, x, sizes)
     end
  =#
     dNumber = convert(UInt8, diagN)
+    dNumber = (dNumber == UInt8(First) || dNumber == UInt8(Second)) ? dNumber : (dNumber < UInt8(First)) ? UInt8(First) : UInt8(Second)
     nBlocks = convert(UInt16, nblocks)
     X_ = convert(Array{Matrix{Float64}}, x)
     bSizes = convert(Array{Tuple{UInt16, UInt16}}, sizes)
 
-    indices = Array{Tuple{UInt16, UInt16}}(undef, nBlocks)
+    indices = Array{Tuple{UInt16, UInt16}, 1}(undef, nBlocks)
     for i = 1 : nBlocks
-        indices[i] = (i, i)
+        indices[i] = (i, dNumber == UInt8(First) ? i : i + 1)
     end
 
     return DiagonalMPS(dNumber, nBlocks, X_, indices, bSizes)
 end
 
 """
-diagMPS = RandomDiagonalMPS(1, 2, [(3, 4), (2, 5)])
+diagMPS = RandomDiagonalMPS(1, 2, [(2, 2), (2, 3)])
 
 Constructs a new DiagonalMPS structure with diagonal number 1 and 2 sub-blocks:
-the first one of size 3 X 4, the second one of size 2 X 5.
-Elements in the sub-blocks are set to random floating point values.
+the first one of size 2 X 2, the second one of size 2 X 3.
+Elements in the sub-blocks are set to random floating point values and
+indices in the BlockIndex array are set to (i, i) if diagonal number is 1
+(i.e. (1, 1), (2, 2), ...) or (i, i+1) otherwise (i.e. (1, 2), (2, 3), ...).
 """
-function RandomDiagonalMPS(diagN, nblocks, sizes,id)
+function RandomDiagonalMPS(diagN, nblocks, sizes)
 #=     if !isinteger(diagN)
         error("$(typeof(diagN)) is not accepted as DiagNumber attribute. It should be a positive integer.")
         return nothing
@@ -134,15 +147,59 @@ function RandomDiagonalMPS(diagN, nblocks, sizes,id)
     end
  =#
     dNumber = convert(UInt8, diagN)
+    dNumber = (dNumber == UInt8(First) || dNumber == UInt8(Second)) ? dNumber : (dNumber < UInt8(First)) ? UInt8(First) : UInt8(Second)
     nBlocks = convert(UInt16, nblocks)
     bSizes = convert(Array{Tuple{UInt16, UInt16}}, sizes)
-    indices=convert(Array{Tuple{Int64,Int64},1},id)
 
     X_ = Array{Matrix{Float64}}(undef, nBlocks)
-    #indices = Array{Tuple{UInt16, UInt16}}(undef, nBlocks)
+    indices = Array{Tuple{UInt16, UInt16}, 1}(undef, nBlocks)
 
     for i = 1 : nBlocks
-        #indices[i] = (i, i)
+        indices[i] = (i, dNumber == UInt8(First) ? i : i + 1)
+        X_[i] = rand(bSizes[i][1], bSizes[i][2])
+    end
+
+    return DiagonalMPS(dNumber, nBlocks, X_, indices, bSizes)
+end
+
+"""
+diagMPS = RandomDiagonalMPS(1, 2, [(2, 2), (2, 3)], [(1, 1), (2, 2)])
+
+Constructs a new DiagonalMPS structure with diagonal number 1 and 2 sub-blocks:
+the first one of size 2 X 2, the second one of size 2 X 3.
+Elements in the sub-blocks are set to random floating point values and
+indices in the BlockIndex array are set to the indices specified as fourth argument.
+"""
+function RandomDiagonalMPS(diagN, nblocks, sizes, index)
+#=     if !isinteger(diagN)
+        error("$(typeof(diagN)) is not accepted as DiagNumber attribute. It should be a positive integer.")
+        return nothing
+    end
+
+    if !isinteger(nblocks)
+        error("$(typeof(nblocks)) is not accepted as NBlocks attribute. It should be a positive integer.")
+        return nothing
+    end
+
+    if !isa(Array{Tuple{UInt, UInt}}, sizes)
+        error("$(typeof(sizes)) is not accepted as BlockSizes attribute. It should be an array of positive integer tuples.")
+        return nothing
+    end
+
+    if !isa(Array{Tuple{UInt, UInt}}, index)
+        error("$(typeof(index)) is not accepted as BlockIndex attribute. It should be an array of positive integer tuples.")
+        return nothing
+    end
+ =#
+    dNumber = convert(UInt8, diagN)
+    dNumber = (dNumber == UInt8(First) || dNumber == UInt8(Second)) ? dNumber : (dNumber < UInt8(First)) ? UInt8(First) : UInt8(Second)
+    nBlocks = convert(UInt16, nblocks)
+    bSizes = convert(Array{Tuple{UInt16, UInt16}}, sizes)
+    indices = convert(Array{Tuple{UInt16, UInt16}, 1}, index)
+
+    X_ = Array{Matrix{Float64}}(undef, nBlocks)
+
+    for i = 1 : nBlocks
         X_[i] = rand(bSizes[i][1], bSizes[i][2])
     end
 
@@ -175,6 +232,7 @@ function SetDiagonalBlock!(diagMPS::DiagonalMPS, i, size, NewX)
     index = convert(UInt16, i)
     newSize = convert(Tuple{UInt16, UInt16}, size)
     newX = convert(Matrix{Float64}, NewX)
+
     diagMPS.X[index] = newX
     diagMPS.BlockSizes[index] = newSize
 
@@ -223,6 +281,7 @@ function SetDiagonalBlockElement!(diagMPS::DiagonalMPS, i, coords, value)
     index = convert(UInt16, i)
     c = convert(Tuple{UInt16, UInt16}, coords)
     val = convert(Float64, value)
+
     diagMPS.X[index][c[1]][c[2]] = val
 
     nothing
@@ -252,76 +311,54 @@ function GetDiagonalBlockElement(diagMPS::DiagonalMPS, i, coords)
 end
 
 """
-DiagonalQR!(diagMPS, 1)
-
-Computes the QR factorization over the first sub-block, overwriting the X[1]
-values with the Q[1] orthogonal matrix values and returning the R[1] upper
-triangular matrix.
-"""
-function DiagonalQR!(diagMPS::DiagonalMPS, i)
-#=     if !isinteger(i)
-        error("$(typeof(i)) is not accepted as index. It should be a positive integer.")
-        return nothing
-    end
- =#
-
-    index = convert(UInt16, i)
-
-    F = qr(diagMPS.X[index])
-    diagMPS.X[index] = Matrix(F.Q)
-
-    return Matrix(F.R)
-end
-
-"""
-DiagonalRXMultiplication!(diagMPS, R, 2)
-
-Multiplies the provided R upper triangular matrix with the X[2], overwriting the X[2]
-values with the result of the multiplication.
-"""
-function DiagonalRXMultiplication!(diagMPS::DiagonalMPS, R, i)
-#=     if !isinteger(i)
-        error("$(typeof(i)) is not accepted as index. It should be a positive integer.")
-        return nothing
-    end
- =#
-
-    R_ = convert(Matrix{Float64}, R)
-    index = convert(UInt16, i)
-
-    diagMPS.X[index] = R_ * diagMPS.X[index]
-
-    nothing
-end
-
-"""
 Author: Siwar Badreddine
-getindices(1,3,6,1)
+indices = GetIndices(1, 3, 6, 1)
 
-Gives back the sub-block indices of the first tensor core X_1[0] for 3 particles and 6 sites
-with respect to the number of particles
+Gives back the sub-block indices of the first tensor core X[1] for 3 particles and 6 sites
+with respect to the number of particles.
 """
-function getindices(i,N,K,diagN)::Array{Tuple{Int64,Int64},1}
+function GetIndices(i, N, K, diagN)::Array{Tuple{UInt16, UInt16}, 1}
+#=     if !isinteger(i)
+        error("$(typeof(i)) is not accepted as index. It should be a positive integer.")
+        return nothing
+    end
 
+    if !isinteger(N)
+        error("$(typeof(N)) is not accepted as particle number. It should be a positive integer.")
+        return nothing
+    end
+
+    if !isinteger(K)
+        error("$(typeof(K)) is not accepted as site number. It should be a positive integer.")
+        return nothing
+    end
+
+    if !isinteger(diagN)
+        error("$(typeof(diagN)) is not accepted as diagonal number. It should be a positive integer.")
+        return nothing
+    end
+ =#
 
     i  = convert(UInt16, i)
     N  = convert(UInt16, N)
     K  = convert(UInt16, K)
-    diagN  = convert(UInt16, diagN)
-    indices = Array{Tuple{UInt16, UInt16}}(undef)
-    if i < (K-N+1)
-        if i> N
-            indices=[(l-(diagN-1),l-(diagN-1)) for l=diagN:N+1]
+    diagN  = convert(UInt8, diagN)
+    diagN = (diagN == UInt8(First) || diagN == UInt8(Second)) ? diagN : (diagN < UInt8(First)) ? UInt8(First) : UInt8(Second)
+
+    indices = Array{Tuple{UInt16, UInt16}, 1}(undef)
+    if i < (K - N + 1)
+        if i > N
+            indices = [(l - (diagN - 1), l - (diagN - 1)) for l = diagN : N+1]
         else
-            indices=[(l,l) for l=1:i]
+            indices = [(l, l) for l = 1 : i]
         end
     else
-        leq=i-(K-N)+1-(diagN-1)
-        geq=N-(diagN-1)+1
-        if i<=N
-            indices=[(l,l) for l=leq:(N-i+1)+1]
+        leq = i - (K - N) + 1 - (diagN - 1)
+        geq = N - (diagN - 1) + 1
+        if i <= N
+            indices = [(l, l) for l = leq : (N - i + 1) + 1]
         else
-            indices=[(l,l) for l=leq:geq]
+            indices = [(l, l) for l = leq : geq]
         end
     end
 
